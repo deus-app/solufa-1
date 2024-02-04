8fcf9a2d9b37f2214e6fc6016e509f4b4e8b238c
bondrewd-iniad <85666399+bondrewd-iniad@users.noreply.github.com>
2024-02-04 19:27:44 +0900

    Initialize gh-pages

---
+++ _next/static/RabjjrnzDPndJQAkjiLdT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/39f2bae3b7eb822c.css","static/chunks/pages/index-fd9d232c6616a0b9.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
